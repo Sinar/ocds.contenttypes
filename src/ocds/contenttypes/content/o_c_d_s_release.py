8b56898bb60c5770a42bb14b042c89215526200f
Khairil Yusof <khairil.yusof@gmail.com>
2020-03-27 18:05:26 +0800

    Add notes feature

--- src/ocds/contenttypes/content/o_c_d_s_release.py
+++ src/ocds/contenttypes/content/o_c_d_s_release.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
+from plone.app.textfield import RichText
 from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
@@ -9,11 +9,11 @@ from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.supermodel.directives import fieldset
+from collective import dexteritytextindexer
+
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
 from zope.interface import implementer
-
-
 from ocds.contenttypes import _
 
 
@@ -30,6 +30,15 @@ class IOCDSRelease(model.Schema):
 
     # Parties
     # OCDS Party roles implemented as relationships
+
+    dexteritytextindexer.searchable('notes')
+    notes = RichText(
+         title=_(u'Notes'),
+         description=_(u'''
+            Notes about this procurement process
+            '''),
+         required=False
+     )
 
     # buyer
 
