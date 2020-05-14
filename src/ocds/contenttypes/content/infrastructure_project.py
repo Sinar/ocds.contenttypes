f736dc6a390316948b3273490fee34d37a0ef275
Khairil Yusof <khairil.yusof@gmail.com>
2020-05-14 14:45:56 +0800

    Additional fields for OC4IDS Infrastructure Project

--- src/ocds/contenttypes/content/infrastructure_project.py
+++ src/ocds/contenttypes/content/infrastructure_project.py
@@ -6,11 +6,11 @@ from plone.dexterity.content import Container
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from ocds.contenttypes import _
+from ocds.contenttypes import _
 
 
 class IInfrastructureProject(model.Schema):
@@ -20,6 +20,73 @@ class IInfrastructureProject(model.Schema):
     # and customize it in Python:
 
     # model.load('infrastructure_project.xml')
+
+    title = schema.TextLine(
+                title=_(u'Title'),
+                description=_(u'''
+                The title of the project.
+                '''),
+                required=True,
+                )
+
+    description = schema.Text(
+                title=_(u'Description'),
+                description=_(u'''
+                A description of this project. This should usually be no
+                longer than a single paragraph.
+                '''),
+                required=False,
+                )
+
+    id_reference = schema.TextLine(
+                title=_(u'Identifier or Reference'),
+                description=_(u'''
+                A unique identifier or reference number for this
+                infrastructure project.
+                '''
+                              ),
+                required=False,
+                )
+
+    # status
+    # https://standard.open-contracting.org/infrastructure/latest/en/reference/codelists/#projectstatus
+
+    # Project Period as fieldset
+    # period_start_date
+    # period_end_date
+    # period_maxExtentDate
+    # period_durationInDays
+
+    # Project Sector
+    # https://standard.open-contracting.org/infrastructure/latest/en/reference/codelists/#projectsector
+
+    purpose = schema.Text(
+            title=_(u'Project purpose'),
+            description=_(u'The socioeconomic purpose of this project'),
+            required=False,
+            )
+
+    # additionalClassifications
+
+    # project_type
+
+    # AssetLifeTime
+    # assetLifeTime_start_date
+    # assetLifeTime_end_date
+    # assetLifeTime_maxExtentDate
+    # assetLifeTime_durationInDays
+
+    # locations
+
+    # budget
+    # budget_currency
+    # budget_amount
+   
+
+    # Parties
+
+    
+    
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
@@ -55,7 +122,6 @@ class IInfrastructureProject(model.Schema):
     #     title=_(u'Secret Notes (only for site-admins)'),
     #     required=False
     # )
-
 
 @implementer(IInfrastructureProject)
 class InfrastructureProject(Container):
