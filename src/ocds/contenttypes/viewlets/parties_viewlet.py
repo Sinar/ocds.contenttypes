833ae3e499a5325787c9afdaa7365f4954f26c76
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-07 18:56:01 +0800

    Initial parties viewlet view Register Infrastructure View as default view for Infrastructure Projects content type

--- src/ocds/contenttypes/viewlets/parties_viewlet.py
+++ src/ocds/contenttypes/viewlets/parties_viewlet.py
@@ -1,15 +1,48 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from zope.schema.interfaces import IVocabularyFactory
 
 
 class PartiesViewlet(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
+    def backrefs(self, attribute_name, portal_type):
+        """
+        Return back references from source object on specified attribute_name
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
 
-    def get_message(self):
-        return u'My message'
+        source_object = self.context
+        result = []
+
+        for rel in catalog.findRelations(
+            dict(to_id=intids.getId(aq_inner(source_object)),
+                 from_attribute=attribute_name)
+              ):
+
+            obj = intids.queryObject(rel.from_id)
+
+            if obj is not None and checkPermission('zope2.View', obj):
+                if obj.portal_type == portal_type:
+
+                    # Temporary workaround for broken objects
+                    if obj.absolute_url():
+                        result.append(obj)
+
+        return result
+
+    def infra(self, attribute_name, portal_type='Infrastructure Project'):
+        return self.backrefs(attribute_name, portal_type)
+
+    # untested
+    def release(self, attribute_name, portal_type='OCDS Release'):
+        return self.backrefs(attribute_name, portal_type)
 
     def render(self):
         return super(PartiesViewlet, self).render()
