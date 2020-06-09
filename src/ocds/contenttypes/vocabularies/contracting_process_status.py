c1c0c825e3b9f8e8b89910bc0d3499a715f3b5f2
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-09 21:48:53 +0800

    ContractingProcessStatus vocab

--- src/ocds/contenttypes/vocabularies/contracting_process_status.py
+++ src/ocds/contenttypes/vocabularies/contracting_process_status.py
@@ -25,8 +25,9 @@ class ContractingProcessStatus(object):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u'sony-a7r-iii', _(u'Sony Aplha 7R III')),
-            VocabItem(u'canon-5d-iv', _(u'Canon 5D IV')),
+            VocabItem(u'pre-award', _(u'Pre-Award')),
+            VocabItem(u'active', _(u'Active')),
+            VocabItem(u'closed', _(u'Closed')),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.
