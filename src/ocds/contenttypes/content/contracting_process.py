b1dff230d83b4f205d1e5d6ef60b4be830b47b7f
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-09 21:46:07 +0800

    Initial fields for Contracting Process

--- src/ocds/contenttypes/content/contracting_process.py
+++ src/ocds/contenttypes/content/contracting_process.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
+from collective import dexteritytextindexer
 # from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-# from ocds.contenttypes import _
+from ocds.contenttypes import _
 
 
 class IContractingProcess(model.Schema):
@@ -20,6 +21,35 @@ class IContractingProcess(model.Schema):
     # and customize it in Python:
 
     # model.load('contracting_process.xml')
+
+    dexteritytextindexer.searchable('title')
+    title = schema.TextLine(
+                title=_(u'Title'),
+                description=_(u'''
+                The formal name of this contracting process. Once set,
+                this should not normally by changed.
+                '''),
+                required=True,
+                )
+    dexteritytextindexer.searchable('description')
+    description = schema.Text(
+                title=_(u'Description'),
+                description=_(u'''
+                The description should summarise the purpose of this
+                contract and the scope of work to be undertaken.
+                '''),
+                required=False,
+                )
+
+    ExternalReference = schema.TextLine(
+                title=_(u'External Reference'),
+                description=_(u'''
+                If this contracting process is identified by some
+                external reference number it may be recorded here.
+                '''),
+                required=False,
+                )
+
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
