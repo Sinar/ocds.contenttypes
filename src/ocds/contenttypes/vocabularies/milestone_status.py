4b7d3bc7f1838b3b32b0630545a7b455100117ea
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-10 21:19:10 +0800

    Add Milestone codes

--- src/ocds/contenttypes/vocabularies/milestone_status.py
+++ src/ocds/contenttypes/vocabularies/milestone_status.py
@@ -25,8 +25,10 @@ class MilestoneStatus(object):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u'sony-a7r-iii', _(u'Sony Aplha 7R III')),
-            VocabItem(u'canon-5d-iv', _(u'Canon 5D IV')),
+            VocabItem(u'scheduled', _(u'Scheduled')),
+            VocabItem(u'met', _(u'Met')),
+            VocabItem(u'notMet', _(u'Not met')),
+            VocabItem(u'partiallyMet', _(u'Partially met')),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.
