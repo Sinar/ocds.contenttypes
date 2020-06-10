6c7c04f3f4c142e0acedcd57c27e2424d9239f1d
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-10 20:48:59 +0800

    Add Procurement Category codes

--- src/ocds/contenttypes/vocabularies/procurement_category.py
+++ src/ocds/contenttypes/vocabularies/procurement_category.py
@@ -25,8 +25,9 @@ class ProcurementCategory(object):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u'sony-a7r-iii', _(u'Sony Aplha 7R III')),
-            VocabItem(u'canon-5d-iv', _(u'Canon 5D IV')),
+            VocabItem(u'goods', _(u'Goods')),
+            VocabItem(u'works', _(u'Works')),
+            VocabItem(u'services', _(u'Services')),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.
