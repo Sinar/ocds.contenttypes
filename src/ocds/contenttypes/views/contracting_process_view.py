9d06321dcbc70dd4565bb5222c2597de768e989d
Khairil Yusof <khairil.yusof@gmail.com>
2020-06-11 09:58:11 +0800

    "Add view: contracting-process-view"

---
+++ src/ocds/contenttypes/views/contracting_process_view.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from ocds.contenttypes import _
+from plone.dexterity.browser.view import DefaultView
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class ContractingProcessView(DefaultView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('contracting_process_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return super(ContractingProcessView, self).__call__()
