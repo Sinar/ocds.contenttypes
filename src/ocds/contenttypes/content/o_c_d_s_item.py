9ba4c39103b51107e88151dfc4e63926d4905cba
Khairil Yusof <khairil.yusof@gmail.com>
2020-08-13 16:16:06 +0800

    Add OCDS Item for Awards and allow OCDS Document to be added to OCDS release

--- src/ocds/contenttypes/content/o_c_d_s_item.py
+++ src/ocds/contenttypes/content/o_c_d_s_item.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.autoform import directives
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from collective import dexteritytextindexer
+from zope import schema
 from zope.interface import implementer
 
 
-# from ocds.contenttypes import _
+from ocds.contenttypes import _
 
 
 class IOCDSItem(model.Schema):
@@ -20,6 +21,46 @@ class IOCDSItem(model.Schema):
     # and customize it in Python:
 
     # model.load('o_c_d_s_item.xml')
+    
+    dexteritytextindexer.searchable('title')
+    title = schema.TextLine(
+                title=_(u'Title'),
+                description=_(u'''
+                Name of item associted with tender, award or contract
+                '''),
+                required=True,
+                )
+    dexteritytextindexer.searchable('description')
+    description = schema.Text(
+                title=_(u'Description'),
+                description=_(u'''
+                A description of the goods, services to be provided.
+                '''),
+                required=False,
+                )
+
+    # Classificaiton 
+    # TODO
+    
+    # Quantity
+
+    quantity = schema.Float(
+            title=_(u'Quantity'),
+            description=_(u'The number of units to be provided.'),
+            required=False,
+            )
+
+    unit = schema.TextLine(
+            title=_(u'Unit'),
+            description=_(u'''
+            A description of the unit in which the supplies, services or
+            works are provided (e.g. hours, kilograms) and the
+            unit-price.
+            '''),
+            required=False,
+            )
+
+    # Unit
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
