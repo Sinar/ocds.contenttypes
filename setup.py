7e5c7855c2f8f7e9b21c562393f15f75b18bee81
Khairil Yusof <khairil.yusof@gmail.com>
2026-08-27 18:46:49 +0800

    deps: use renamed politikus.popolo package
    popolo.contenttypes was renamed to politikus.popolo; update the
    install_requires and the viewlet/test references to the new module
    path.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         'plone.api>=1.8.4',
         'plone.restapi',
         'plone.app.dexterity',
-        'popolo.contenttypes',
+        'politikus.popolo',
     ],
     extras_require={
         'test': [
